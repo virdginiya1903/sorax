2a2f31e59308285cbee3b28de5626803066da1a9
virdginiya1903 <virdginiya1903@mail.ru>
2019-08-10 19:23:58 +0300

    условный оператор

--- index.js
+++ index.js
@@ -11,28 +11,33 @@ if (n < 3) {
   console.log("не n");
 }
 
-// let name = "Gulnara",
-//   sity;
-// if (name === "Guzel") {
-//   sity = "Moscow";
-// }
-// if (name === "Gulnara") {
-//   sity = "Blag";
-// }
-// console.log(sity);
-let name = "Guzel",
+let name = "Gulnara",
   sity;
-switch (name) {
-  // выражение: инструкции
-  case "Guzel":
-    sity = "Moscow";
-    break;
-  case "Gulnara":
-    sity = "Blag";
-    break;
-  default:
-    sity = "Ufa";
+if (name === "Guzel") {
+  sity = "Moscow";
+}
+if (name === "Gulnara") {
+  sity = "Blag";
 }
 console.log(sity);
 
+let name1 = "Guzel",
+  sity1;
+switch (name1) {
+  // выражение: инструкции
+  case "Guzel":
+    sity1 = "Moscow";
+    break;
+  case "Gulnara":
+    sity1 = "Blag";
+    break;
+  default:
+    sity1 = "Ufa";
+}
+console.log(sity1);
+
 //Условный оператор
+// выражение1 ? выражение2 : выражение3
+let x = 15;
+let text = x > 10 ? "x больше 10 " : "x меньше или равно 10";
+console.log(text);
